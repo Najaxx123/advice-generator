b5b80dcdf904aea6b216ba2da3876d2540d77528
Najaxx <romain.brival@outlook.fr>
2025-06-02 16:00:53 +0200

    i need to continue it later but not for now

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const adviseZone = document.querySelector(".advice")
+const adviceId = document.querySelector(".advice-id")
+
+fetch("https://api.adviceslip.com/advice")
+
+.then (function(res) {
+    return res.json()
+})
+
+.then (function(result) {
+    adviseZone.textContent = result.slip.advice
+    adviceId.textContent = result.slip.id
+    console.log(result)
+})
+.catch (function(err) {
+    console.log("une erreur est la mon chef" ,err);
+})
